80e33038e648a001c651ee280630296e0ea1de06
Ndricim Rrapi <ndricim.rrapi@gmail.com>
2023-02-17 20:23:45 +0100

    Prettify code with prettier

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,18 +7,18 @@ import { FEMALE_NAMES_MAX_LENGTH } from 'lists/female';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "app";
-  GENERATE_BUTTON_ID = "generateButton";
-  RANDOM_NAMES_RESULTS_PARAGRAPH = "resultsParagraph";
-  NUMBER_OF_NAMES_DESIRED_INPUT = "numberOfNamesDesiredInputField";
+  title = 'app';
+  GENERATE_BUTTON_ID = 'generateButton';
+  RANDOM_NAMES_RESULTS_PARAGRAPH = 'resultsParagraph';
+  NUMBER_OF_NAMES_DESIRED_INPUT = 'numberOfNamesDesiredInputField';
   numberOfNamesDesired = 2;
   MAX_RANDOM_NAMES_ALLOWED = 10;
-  genders = ["Female", "Male", "Both"];
-  chosenGender = "Female";
-  generatedRandomNames = [""];
+  genders = ['Female', 'Male', 'Both'];
+  chosenGender = 'Female';
+  generatedRandomNames = [''];
 
   ngOnInit() {
     this.generateRandomNames();
@@ -26,7 +26,7 @@ export class AppComponent {
 
   /**
    * Generates and returns a random number between min (inclusive) and max (exclusive)
-   * @param min Minimum number in range to generate 
+   * @param min Minimum number in range to generate
    * @param max Maximum number in range to generate
    * @returns Random number between "min" and "max"
    */
@@ -37,49 +37,56 @@ export class AppComponent {
   }
 
   /**
-   * Predicate to check gender is female 
+   * Predicate to check gender is female
    * @returns if chosen gender is female or not
    */
-  isGenderFemale(){
-    return this.chosenGender === "Female" ? true : false;
+  isGenderFemale() {
+    return this.chosenGender === 'Female' ? true : false;
   }
 
   /**
-   * Predicate to check gender is male 
+   * Predicate to check gender is male
    * @returns if chosen gender is male or not
    */
-  isGenderMale(){
-    return this.chosenGender === "Male" ? true : false;
+  isGenderMale() {
+    return this.chosenGender === 'Male' ? true : false;
   }
 
   /**
    * Generates a desired number of random names from the given "randomNamesList" of names
    * provided a chosen gender
    */
-  generateRandomNames(){
+  generateRandomNames() {
     if (this.numberOfNamesDesired > this.MAX_RANDOM_NAMES_ALLOWED) {
-      alert("Maximum number of allowed random names 10");
+      alert('Maximum number of allowed random names 10');
       return;
     }
 
-    let randomNamesTempList :string[] = [];
-    const maxIndex = this.isGenderFemale() ? FEMALE_NAMES_MAX_LENGTH - 1 : MALE_NAMES_MAX_LENGTH - 1;
-    if (this.isGenderFemale()){
+    let randomNamesTempList: string[] = [];
+    const maxIndex = this.isGenderFemale()
+      ? FEMALE_NAMES_MAX_LENGTH - 1
+      : MALE_NAMES_MAX_LENGTH - 1;
+    if (this.isGenderFemale()) {
       for (let index = 0; index < this.numberOfNamesDesired; index++) {
         const randomIndex = this.getRandomArbitrary(0, maxIndex);
-        randomNamesTempList.push(fullListFemaleNames[randomIndex])
+        randomNamesTempList.push(fullListFemaleNames[randomIndex]);
       }
-    } else if (this.isGenderMale()){
+    } else if (this.isGenderMale()) {
       for (let index = 0; index < this.numberOfNamesDesired; index++) {
         const randomIndex = this.getRandomArbitrary(0, maxIndex);
-        randomNamesTempList.push(fullListMaleNames[randomIndex])
+        randomNamesTempList.push(fullListMaleNames[randomIndex]);
       }
     } else {
-      const maxIndex = FEMALE_NAMES_MAX_LENGTH > MALE_NAMES_MAX_LENGTH ? FEMALE_NAMES_MAX_LENGTH : MALE_NAMES_MAX_LENGTH ;
+      const maxIndex =
+        FEMALE_NAMES_MAX_LENGTH < MALE_NAMES_MAX_LENGTH
+          ? FEMALE_NAMES_MAX_LENGTH
+          : MALE_NAMES_MAX_LENGTH;
       for (let index = 0; index < this.numberOfNamesDesired; index++) {
         const randomIndex = this.getRandomArbitrary(0, maxIndex);
         const isFemale = this.getRandomArbitrary(0, 1);
-        isFemale ? randomNamesTempList.push(fullListFemaleNames[randomIndex]) : randomNamesTempList.push(fullListMaleNames[randomIndex]);
+        isFemale
+          ? randomNamesTempList.push(fullListFemaleNames[randomIndex])
+          : randomNamesTempList.push(fullListMaleNames[randomIndex]);
       }
     }
     this.generatedRandomNames = randomNamesTempList;
