b69ad8e06a8b41de24a1752d9136d7b036fd18d3
Ndricim Rrapi <ndricim.rrapi@gmail.com>
2023-10-01 17:30:50 +0200

    add nav bar - structure

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppComponent } from './app.component';
 import { UserGeneratorComponent } from './user-generator/user-generator.component';
 import { NameGeneratorComponent } from './name-generator/name-generator.component';
 import { AppRoutingModule } from './modules/app-routing.module';
-import { PageSwitcherComponent } from './page-switcher/page-switcher.component';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -21,7 +21,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppComponent,
     UserGeneratorComponent,
     NameGeneratorComponent,
-    PageSwitcherComponent,
+    NavigationBarComponent,
   ],
   imports: [
     BrowserModule,
