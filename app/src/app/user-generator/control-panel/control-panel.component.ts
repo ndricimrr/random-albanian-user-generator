293a976fb474186586d6a0b26dcc081de09407f9
Ndricim Rrapi <ndricim.rrapi@gmail.com>
2023-11-07 02:55:14 +0100

    Save progress

--- app/src/app/user-generator/control-panel/control-panel.component.ts
+++ app/src/app/user-generator/control-panel/control-panel.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ViewType } from 'src/app/utils/component-interface';
 import { Gender, User } from 'src/app/utils/user-interface';
 import { fullListCounties } from 'lists/counties';
 import { TranslateService } from '@ngx-translate/core';
+import { MAX_AGE_ALLOWED, MAX_RANDOM_NAMES_ALLOWED } from '../constants';
 
 @Component({
   selector: 'control-panel',
@@ -16,13 +17,22 @@ export class ControlPanelComponent {
     ViewType.GRAPHICAL_TABLE,
     ViewType.GRAPHICAL_LIST,
   ];
-  chosenGender = Gender.FEMALE;
-  chosenViewType: ViewType = ViewType.JSON;
-  numberOfNamesDesired = 2;
-  MAX_RANDOM_NAMES_ALLOWED = 10;
-  MAX_AGE_ALLOWED = 100;
-  ageDesired = 18;
-  chosenCounty = fullListCounties[0];
+
+  @Input() chosenGender: string = Gender.FEMALE;
+  @Output() chosenGenderChange = new EventEmitter<string>();
+
+  @Input() chosenViewType: string = ViewType.JSON;
+  @Output() chosenViewTypeChange = new EventEmitter<string>();
+
+  @Input() chosenNumberOfUsers: number = 2;
+  @Output() chosenNumberOfUsersChange = new EventEmitter<number>();
+
+  @Input() chosenCounty: string = fullListCounties[0];
+  @Output() chosenCountyChange = new EventEmitter<string>();
+
+  @Input() ageDesired: number = 18;
+  @Output() ageDesiredChange = new EventEmitter<number>();
+
   counties = fullListCounties;
   translate: TranslateService = TranslateService.prototype;
 
@@ -30,5 +40,25 @@ export class ControlPanelComponent {
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('sq');
     this.translate = translate;
+  }
+
+  onChosenGenderChange() {
+    this.chosenGenderChange.emit(this.chosenGender);
+  }
+
+  onChosenViewTypeChange() {
+    this.chosenViewTypeChange.emit(this.chosenViewType);
+  }
+
+  onChosenNumUsersChange() {
+    this.chosenNumberOfUsersChange.emit(this.chosenNumberOfUsers);
+  }
+
+  onChosenCountyChange() {
+    this.chosenCountyChange.emit(this.chosenCounty);
+  }
+
+  onDesiredAgeChange() {
+    this.ageDesiredChange.emit(this.ageDesired);
   }
 }
