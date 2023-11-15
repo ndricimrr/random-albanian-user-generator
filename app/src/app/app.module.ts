6a0eae4c97a4a2d3d303658e369a5d997c7e4982
ndricimrr <ndricimrr@gmail.com>
2023-11-15 22:52:38 +0100

    Add first version codemirror

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { UserGeneratorComponent } from './user-generator/user-generator.component';
-import { NameGeneratorComponent } from './name-generator/name-generator.component';
-import { AppRoutingModule } from './modules/app-routing.module';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { FootNavComponent } from './foot-nav/foot-nav.component';
-import { GridTableViewComponent } from './user-generator/grid-table-view/users-grid-table-view.component';
-import { UserTableViewComponent } from './user-generator/grid-table-view/user-table-view/user-table-view.component';
-import { ControlPanelComponent } from './user-generator/control-panel/control-panel.component';
-import { ListViewComponent } from './user-generator/list-view/list-view.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { UserGeneratorComponent } from "./user-generator/user-generator.component";
+import { NameGeneratorComponent } from "./name-generator/name-generator.component";
+import { AppRoutingModule } from "./modules/app-routing.module";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { FootNavComponent } from "./foot-nav/foot-nav.component";
+import { GridTableViewComponent } from "./user-generator/grid-table-view/users-grid-table-view.component";
+import { UserTableViewComponent } from "./user-generator/grid-table-view/user-table-view/user-table-view.component";
+import { ControlPanelComponent } from "./user-generator/control-panel/control-panel.component";
+import { ListViewComponent } from "./user-generator/list-view/list-view.component";
+import { CodeViewComponent } from "./user-generator/code-view/code-view.component";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -32,6 +33,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     UserTableViewComponent,
     ControlPanelComponent,
     ListViewComponent,
+    CodeViewComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,7 +42,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule,
     NgbModule,
     TranslateModule.forRoot({
-      defaultLanguage: 'sq',
+      defaultLanguage: "sq",
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
