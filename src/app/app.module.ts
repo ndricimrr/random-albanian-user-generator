c6bac2e455562bd9bb6e5b4d1280578341c9a1e1
ndricimrr <ndricimrr@gmail.com>
2024-01-23 00:27:55 +0100

    Test deployment new 14 - add assets to folder angular json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,19 +20,19 @@ import { APP_BASE_HREF, CommonModule } from "@angular/common";
 import { InjectionToken } from "@angular/core";
 
 // AoT requires an exported function for factories
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http);
-// }
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 // export const BASE_HREF = new InjectionToken<string>("BASE_HREF");
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(
-    http,
-    "/random-albanian-user-generator/assets/i18n/",
-    ".json"
-  );
-}
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(
+//     http,
+//     "/random-albanian-user-generator/assets/i18n/",
+//     ".json"
+//   );
+// }
 
 @NgModule({
   declarations: [
