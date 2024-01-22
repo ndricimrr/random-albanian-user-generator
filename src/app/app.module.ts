5978b7c551ede77c676531e2c91d0e6e6204e9d9
ndricimrr <ndricimrr@gmail.com>
2024-01-23 00:07:30 +0100

    Test deployment new 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,15 +20,15 @@ import { APP_BASE_HREF, CommonModule } from "@angular/common";
 import { InjectionToken } from "@angular/core";
 
 // AoT requires an exported function for factories
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http);
-// }
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 export const BASE_HREF = new InjectionToken<string>("BASE_HREF");
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, BASE_HREF + "/assets/i18n/", ".json");
-}
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, BASE_HREF + "/assets/i18n/", ".json");
+// }
 
 @NgModule({
   declarations: [
