894a2f29026adc3ad697739ac98472a66b67152e
ndricimrr <ndricimrr@gmail.com>
2024-01-23 01:11:47 +0100

    Test deployment new 22

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-import { NameGeneratorComponent } from '../name-generator/name-generator.component';
-import { UserGeneratorComponent } from '../user-generator/user-generator.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router"; // CLI imports router
+import { NameGeneratorComponent } from "../name-generator/name-generator.component";
+import { UserGeneratorComponent } from "../user-generator/user-generator.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'generate-users', pathMatch: 'full' },
-  { path: 'generate-names', component: NameGeneratorComponent },
-  { path: 'generate-users', component: UserGeneratorComponent },
+  { path: "", redirectTo: "generate-users", pathMatch: "full" },
+  { path: "generate-names", component: NameGeneratorComponent },
+  { path: "generate-users", component: UserGeneratorComponent },
 ];
 
 // configures NgModule imports and exports
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-navigation-bar',
-  templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css'],
+  selector: "app-navigation-bar",
+  templateUrl: "./navigation-bar.component.html",
+  styleUrls: ["./navigation-bar.component.css"],
 })
 export class NavigationBarComponent {
   currentLanguage: string;
-  activeLink: string = 'generate-users';
+  activeLink: string = "generate-users";
 
-  constructor(private languageService: TranslateService) {
-    const currentPageLink = window.location.pathname.substring(1) || '';
-    this.activeLink = currentPageLink;
+  constructor(
+    private languageService: TranslateService,
+    private router: Router
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Get the current page link after each navigation
+        const currentPageLink = this.router.url.substring(1) || "";
+        console.log(currentPageLink);
+        this.activeLink = currentPageLink;
+      }
+    });
 
-    const storedLanguage = sessionStorage.getItem('language');
+    const storedLanguage = sessionStorage.getItem("language");
     this.currentLanguage = storedLanguage || languageService.currentLang;
     this.languageService.use(this.currentLanguage);
   }
@@ -22,6 +32,6 @@ export class NavigationBarComponent {
   changeLanguage(language: string): void {
     this.currentLanguage = language;
     this.languageService.use(language);
-    sessionStorage.setItem('language', language);
+    sessionStorage.setItem("language", language);
   }
 }
